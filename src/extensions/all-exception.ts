b68a396a58faaf80468b0562d5af98d51951203e
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2024-04-08 20:03:25 +0800

    Next (#1)
    * for fun

---
+++ src/extensions/all-exception.ts
@@ -0,0 +1,80 @@
+import { ErrorHandler, NotFoundHandler } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { StatusCodes } from "http-status-codes";
+import { ZodError } from "zod";
+import { ProblemDocument } from "http-problem-details";
+import { DetailMessage, detailMessage } from "./constants";
+import defu from "defu";
+
+type ExceptionHandleOption = {
+  development?(): void;
+  messageConstants?: Partial<DetailMessage>;
+};
+
+export function errorHandle(
+  exceptionHandleOption: ExceptionHandleOption = {
+    development() {},
+    messageConstants: {},
+  }
+) {
+  const { development, messageConstants } = exceptionHandleOption;
+  const _messageConstants = defu(messageConstants, detailMessage);
+
+  return ((e, ctx) => {
+    development?.();
+
+    if (e instanceof ZodError) {
+      return ctx.json(
+        new ProblemDocument({
+          detail: _messageConstants["Complete request parameters not provided"],
+          instance: ctx.req.path,
+          status: StatusCodes.BAD_REQUEST,
+        }),
+        StatusCodes.BAD_REQUEST
+      );
+    }
+
+    if (e instanceof HTTPException) {
+      return ctx.json(
+        new ProblemDocument({
+          detail: e.message,
+          instance: ctx.req.path,
+          status: e.status,
+        }),
+        e.status
+      );
+    }
+
+    return ctx.json(
+      new ProblemDocument({
+        detail: _messageConstants["Unknown error please contact to admin"],
+        instance: ctx.req.path,
+        status: StatusCodes.BAD_REQUEST,
+      }),
+      StatusCodes.SERVICE_UNAVAILABLE
+    );
+  }) as ErrorHandler;
+}
+
+export function notFound(
+  exceptionHandleOption: ExceptionHandleOption = {
+    development() {},
+    messageConstants: {},
+  }
+) {
+  const { development, messageConstants } = exceptionHandleOption;
+  const _messageConstants = defu(messageConstants, detailMessage);
+
+  return ((ctx) => {
+    development?.();
+
+    return ctx.json(
+      new ProblemDocument({
+        detail: _messageConstants["Not found"],
+        instance: ctx.req.path,
+        status: StatusCodes.NOT_FOUND,
+      }),
+      StatusCodes.NOT_FOUND
+    );
+  }) as NotFoundHandler;
+}
---
+++ src/extensions/constants.ts
@@ -0,0 +1,8 @@
+export type DetailMessage = typeof detailMessage;
+export const detailMessage = {
+  "Complete request parameters not provided":
+    "Complete request parameters not provided",
+  "Unknown error please contact to admin":
+    "Unknown error please contact to admin",
+  "Not found": "Not found",
+};
